f5bb229b272443115aed7302d21d13764d33e1e9
Andreas Kleven <andreas.kleven@live.no>
2018-01-21 16:28:28 +0100

    Fixed build

--- build.bat
+++ build.bat
@@ -6,4 +6,7 @@ cd "C:\Users\Andyhk\Documents\visual studio 2017\Projects\AndyOS\"
 @SET BootBinary="Root\boot\andyos.bin"
 @SET ISOName="AndyOS.iso"
 
+%Rebase% 0x100000 %OS%
+copy %OS% %BootBinary% /y
+
 IF exist %windir%\sysnative\bash.exe (%windir%\sysnative\bash.exe -c "grub-mkrescue -o %ISOName% Root/") ELSE (bash.exe -c "grub-mkrescue -o %ISOName% Root/" & pause)
