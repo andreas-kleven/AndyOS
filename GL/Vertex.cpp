7422f12235b604e035cebb6b7403d223bae295f3
Andreas Kleven <andreas.kleven@live.no>
2018-05-27 12:34:26 +0200

    Vertex multiplication

--- GL/Vertex.cpp
+++ GL/Vertex.cpp
@@ -11,6 +11,7 @@ Vertex::Vertex(float x, float y, float z, ColRGB col, Vector4 normal) : Vertex()
 	this->pos.x = x;
 	this->pos.y = y;
 	this->pos.z = z;
+	this->pos.w = 1;
 
 	this->color = col;
 	this->builtColor = col;
@@ -21,8 +22,5 @@ Vertex::Vertex(float x, float y, float z, ColRGB col, Vector4 normal) : Vertex()
 
 void Vertex::MulMatrix(const Matrix4& mat)
 {
-	tmpPos.x = mat.elems[0] * pos.x + mat.elems[1] * pos.y + mat.elems[2] * pos.z + mat.elems[3];
-	tmpPos.y = mat.elems[4] * pos.x + mat.elems[5] * pos.y + mat.elems[6] * pos.z + mat.elems[7];
-	tmpPos.z = mat.elems[8] * pos.x + mat.elems[9] * pos.y + mat.elems[10] * pos.z + mat.elems[11];
-	tmpPos.w = mat.elems[12] * pos.x + mat.elems[13] * pos.y + mat.elems[14] * pos.z + mat.elems[15];
+	tmpPos = mat * pos;
 }
