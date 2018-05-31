30173f51c1376203cc207e3b495d4df62923f88c
Andreas Kleven <andreas.kleven@live.no>
2018-05-31 17:10:01 +0200

    Test program

---
+++ Test/Source.cpp
@@ -0,0 +1,39 @@
+int main()
+{
+	int result = 0;
+
+	/*//Get time
+	int hour = 0;
+	int minute = 0;
+	int second = 0;
+
+	_asm
+	{
+		mov eax, 3
+		lea ebx, hour
+		lea ecx, minute
+		lea edx, second
+		int 0x80
+		add esp, 4
+
+		mov[result], eax
+	}*/
+
+	//Print
+	//char text[256];
+	//vprintf(text, "%i:%i:%i\n", hour, minute, second);
+
+	const char* text = "Hello\n";
+
+	_asm
+	{
+		mov eax, 2
+		mov ebx, text
+		int 0x80
+		add esp, 4
+
+		mov[result], eax
+	}
+
+	return 2;
+}
