5a45c45954a93c4e8d6b4c0abe461e4395b83180
Andreas Kleven <andreas.kleven@live.no>
2018-08-03 19:29:07 +0200

    Histogram coloring

--- Programs/Mandelbrot/mandelbrot.cpp
+++ Programs/Mandelbrot/mandelbrot.cpp
@@ -5,15 +5,138 @@
 
 using namespace gui;
 
+const int iter_max = 256;
+int histogram[iter_max + 1];
+
 double zoom = 4;
 double ofx = 0;
 double ofy = 0;
 double rot = 0;
 
+int get_iteration(double x, double y, double* z2)
+{
+	double Zx = 0;
+	double Zy = 0;
+	double Zx2 = 0;
+	double Zy2 = 0;
+
+	//Julia set
+	//Zx = x;
+	//Zy = y;
+
+	//x = 0.7885 * cos(rot);
+	//y = 0.7885 * sin(rot);
+	
+	//Optimizations
+	double q = pow(x - 0.25, 2) + y*y;
+	if ((q * (q + (x - 0.25)) < y*y / 4) 	//Inside cardioid
+		|| ((x+1) * (x+1) + y*y) * 16 < 1)	//Inside period-2 bulb
+	{
+		*z2 = 0;
+		return iter_max;
+	}
+
+	Zx2 = Zx*Zx;
+	Zy2 = Zy*Zy;
+
+	int iter = 0;
+	while (iter < iter_max && ((Zx2 + Zy2) < (1 << 16)))
+	{
+		Zy = 2 * Zx*Zy + y;
+		Zx = Zx2 - Zy2 + x;
+		Zx2 = Zx*Zx;
+		Zy2 = Zy*Zy;
+		iter++;
+	}
+
+	*z2 = Zx2 + Zy2;
+	return iter;
+}
+
+typedef struct {
+    double r;       // a fraction between 0 and 1
+    double g;       // a fraction between 0 and 1
+    double b;       // a fraction between 0 and 1
+} rgb;
+
+typedef struct {
+    double h;       // angle in degrees
+    double s;       // a fraction between 0 and 1
+    double v;       // a fraction between 0 and 1
+} hsv;
+
+rgb hsv2rgb(hsv in)
+{
+    double      hh, p, q, t, ff;
+    long        i;
+    rgb         out;
+
+    if(in.s <= 0.0) {       // < is bogus, just shuts up warnings
+        out.r = in.v;
+        out.g = in.v;
+        out.b = in.v;
+        return out;
+    }
+    hh = in.h;
+    if(hh >= 360.0) hh = 0.0;
+    hh /= 60.0;
+    i = (long)hh;
+    ff = hh - i;
+    p = in.v * (1.0 - in.s);
+    q = in.v * (1.0 - (in.s * ff));
+    t = in.v * (1.0 - (in.s * (1.0 - ff)));
+
+    switch(i) {
+    case 0:
+        out.r = in.v;
+        out.g = t;
+        out.b = p;
+        break;
+    case 1:
+        out.r = q;
+        out.g = in.v;
+        out.b = p;
+        break;
+    case 2:
+        out.r = p;
+        out.g = in.v;
+        out.b = t;
+        break;
+
+    case 3:
+        out.r = p;
+        out.g = q;
+        out.b = in.v;
+        break;
+    case 4:
+        out.r = t;
+        out.g = p;
+        out.b = in.v;
+        break;
+    case 5:
+    default:
+        out.r = in.v;
+        out.g = p;
+        out.b = q;
+        break;
+    }
+    return out;     
+}
+
+uint32 get_color(double hue)
+{
+	hsv c;
+	c.h = hue * 360;
+	c.s = 1;
+	c.v = 1;
+
+	rgb rc = hsv2rgb(c);
+
+	return Color(rc.r, rc.g, rc.b).ToInt();
+}
+
 void run(GC& gc)
 {
-	int iter_max = 256;
-
 	int ticks = get_ticks();
 	double delta = 1;
 
@@ -43,147 +166,52 @@ void run(GC& gc)
 		int index = 0;
 		uint32* buf = gc_buf.framebuffer + gc_buf.y * gc_buf.stride + gc_buf.x;
 
-		double Cx;
-		double Cy;
-		double Zx = 0;
-		double Zy = 0;
-		double Zx2 = 0;
-		double Zy2 = 0;
+		//Reset histogram
+		for (int i = 0; i <= iter_max; i++)
+			histogram[i] = 0;
 
 		for (int _y = 0; _y < gc_buf.height; _y++)
 		{
 			for (int _x = 0; _x < gc_buf.width; _x++)
 			{
-				Cx = (_x - gc_buf.width / 2) * asd1 + ofx;
-				Cy = (_y - gc_buf.height / 2) * asd1 + ofy;
-				Zx = 0;
-				Zy = 0;
-				Zx2 = 0;
-				Zy2 = 0;
+				double x = (_x - gc_buf.width / 2) * asd1 + ofx;
+				double y = (_y - gc_buf.height / 2) * asd1 + ofy;
 
-				int iter = 0;
-				/*for (iter = 0; iter < iter_max && ((Zx2 + Zy2) < (1 << 16)); iter++)
-				{
-					Zy = Zx * Zy;
-					Zy += Zy;
-					Zy += cy;
+				double z2;
+				int iter = get_iteration(x, y, &z2);
 
-					Zx = Zx2 - Zy2 + cx;
+				histogram[iter]++;
+			}
+		}
 
-					Zx2 = Zx * Zx;
-					Zy2 = Zy * Zy;
+		for (int _y = 0; _y < gc_buf.height; _y++)
+		{
+			for (int _x = 0; _x < gc_buf.width; _x++)
+			{
+				double x = (_x - gc_buf.width / 2) * asd1 + ofx;
+				double y = (_y - gc_buf.height / 2) * asd1 + ofy;
+				
+				double z2;
+				int iter = get_iteration(x, y, &z2);
 
-					//Zy = Zx * Zy * 2.0 + cy;
-					//Zx = Zx2 - Zy2 + cx;
-					//
-					//Zx2 = Zx * Zx;
-					//Zy2 = Zy * Zy;
-				}*/
+				int total = gc_buf.width * gc_buf.height;
 
-				/*double Zx = cx;
-				double Zy = cy;
+				double hue = 0;
+				for (int i = 0; i <= iter; i++)
+  					hue += histogram[i] / (double)total;
 
-				double Cx = 0.7885 * cos(rot);
-				double Cy = 0.7885 * sin(rot);
+				*buf++ = get_color(hue);
 
-				Zx2 = Zx*Zx;
-				Zy2 = Zy*Zy;*/
-
-				while (iter < iter_max && ((Zx2 + Zy2) < (1 << 16)))
-				{
-					Zy = 2 * Zx*Zy + Cy;
-					Zx = Zx2 - Zy2 + Cx;
-					Zx2 = Zx*Zx;
-					Zy2 = Zy*Zy;
-					iter++;
-				}
-
-				if (iter == iter_max)
+				/*if (iter == iter_max)
 				{
 					*buf++ = 0;
 				}
 				else
 				{
-					//*buf++ = iter;
-					//continue;
-
-					double zn = sqrt(Zx2 + Zy2);
-					double hue = iter + 1.0 - log2(log(zn));  // 2 is escape radius
-					//int gg = clamp((int)(logn(hue, 200) * 0xFF), 0, 0xFF);
-
-					uint32 color = (int)(logn(hue, 200) * 0xFF) % 0xFF;
-
-					//int g = clamp((int)(logn(logn(hue, 5), 5) * 0xFF), 0, 0xFF);
-					//int g2 = clamp((int)(logn(logn(hue, 6), 6) * 0xFF), 0, 0xFF);
-					//color += g2 << 16;
-					//color += g << 8;
-
+					double zn = sqrt(z2);
+					uint32 color = clamp((int)((iter - log2(log(zn) / log(1 << 16))) * 5), 0, 0xFF);
 					*buf++ = color;
-
-					//*buf++ = palette.GetColor(iter);
-					//buf++ = palette.GetColor(color);
-				}
-
-				/*z.real = _x * asd1 - 2;
-				z.img = _y * asd1 - 2;
-
-				int res = test(z);
-
-				if (res == 0)
-				{
-					VBE::SetPixel(_x, _y, 0);
-				}
-				else
-				{
-					//double val = logn(res, count) * 255;
-
-					//uint8 b = clamp((int)((val + frac)), 0, 0xFF);
-					uint8 b = res;
-
-					uint32 c = (b << 16) | (b << 8) | b;
-					VBE::SetPixel(_x, _y, c);
-				}
-
-				index++;
-
-				/*double x0 = _x * asd1 - 2;
-				double y0 = _y * asd1 - 2;
-				double x = 0.0;
-				double y = 0.0;
-				double iteration = 0;
-				int max_iteration = 1000;
-
-				// Here N=2^8 is chosen as a reasonable bailout radius.
-				while (x*x + y*y < (1 << 16) && iteration < max_iteration)
-				{
-					double xtemp = x*x - y*y + x0;
-					y = 2 * x*y + y0;
-					x = xtemp;
-					iteration = iteration + 1;
-				}
-
-				// Used to avoid doubleing point issues with points inside the set.
-				if (iteration < max_iteration)
-				{
-					// sqrt of inner term removed using log simplification rules.
-					double log_zn = log(x*x + y*y) / 2;
-					double nu = log(log_zn / log(2)) / log(2);
-
-					// Rearranging the potential function.
-					// Dividing log_zn by log(2) instead of log(N = 1<<8)
-					// because we want the entire palette to range from the
-					// center to radius 2, NOT our bailout radius.
-					iteration = iteration + 1 - nu;
-				}
-
-				double color1 = (double)floor(iteration);
-				double color2 = (double)floor(iteration) + 1;
-
-				// iteration % 1 = fractional part of iteration.
-				double frac = iteration - (int)iteration;
-
-				uint8 color = (uint8)((color1 + (color2 - color1) * frac));
-				VBE::SetPixel(_x, _y, color);*/
+				}*/
 			}
 
 			index += gc_buf.stride - gc_buf.width;
