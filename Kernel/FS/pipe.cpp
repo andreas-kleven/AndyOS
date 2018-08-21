652daa57ca5c2f60f2130764f05ef979093456cf
Andreas Kleven <andreas.kleven@live.no>
2018-08-21 20:33:15 +0200

    Blocking pipe read and writes

--- Kernel/FS/pipe.cpp
+++ Kernel/FS/pipe.cpp
@@ -1,19 +1,41 @@
 #include "pipe.h"
 #include "string.h"
 
+Event read_event;
+Event write_event;
+
 Pipe::Pipe(int buf_size)
 {
     buffer = CircularDataBuffer(buf_size);
+    write_event.Set();
 }
 
 int Pipe::Read(FILE* file, char* buf, size_t size)
 {
-    return buffer.Read(size, buf);
+    read_event.Wait();
+    int ret = buffer.Read(size, buf);
+
+    if (buffer.IsEmpty())
+        read_event.Clear();
+
+    if (!buffer.IsFull())
+        write_event.Set();
+
+    return ret;
 }
 
 int Pipe::Write(FILE* file, const char* buf, size_t size)
 {
-    return buffer.Write(buf, size);
+    read_event.Set();
+    int ret = buffer.Write(buf, size);
+
+    if (!buffer.IsEmpty())
+        read_event.Set();
+
+    if (buffer.IsFull())
+        write_event.Clear();
+
+    return ret;
 }
 
 int Pipe::Seek(FILE* file, long offset, int origin)
