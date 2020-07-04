1ab1e4c896bf5bc1206814d377233546dc6b15a9
Andreas Kleven <andreas.kleven@live.no>
2020-07-04 12:54:03 +0200

    qemu script bridged network

--- Tools/gdb-x86.sh
+++ Tools/gdb-x86.sh
@@ -2,7 +2,7 @@
 
 GNOME_TERMINAL_SCREEN=""
 mkfifo /tmp/guest
-gnome-terminal -- qemu-system-i386 -cdrom ../AndyOS.iso -s -S -serial pipe:/tmp/guest
+gnome-terminal -- qemu-system-i386 -cdrom ../AndyOS.iso -drive file="/home/andreas/VirtualBox VMs/AndyOS/disk.img",media=cdrom -s -S -serial pipe:/tmp/guest -netdev bridge,id=mynet0 -device e1000,netdev=mynet0,mac=52:55:00:d1:55:01,id=nic1
 
 sleep 0.1
 
