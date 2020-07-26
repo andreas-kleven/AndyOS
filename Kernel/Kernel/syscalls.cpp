148122efee52a495b2046e3ed2591d3fa5310bda
Andreas Kleven <andreas.kleven@live.no>
2020-07-26 15:35:12 +0200

    Syscall handler sys_ prefix

--- Kernel/Kernel/syscalls.cpp
+++ Kernel/Kernel/syscalls.cpp
@@ -53,74 +53,74 @@ namespace Syscalls
 		return Dispatcher::CurrentProcess()->filetable;
 	}
 
-	int open(const char *filename, int flags)
+	int sys_open(const char *filename, int flags)
 	{
 		return VFS::Open(CurrentFiletable(), filename);
 	}
 
-	int close(int fd)
+	int sys_close(int fd)
 	{
 		return VFS::Close(CurrentFiletable(), fd);
 	}
 
-	size_t read(int fd, char *buf, size_t size)
+	size_t sys_read(int fd, char *buf, size_t size)
 	{
 		return VFS::Read(CurrentFiletable(), fd, buf, size);
 	}
 
-	size_t write(int fd, const char *buf, size_t size)
+	size_t sys_write(int fd, const char *buf, size_t size)
 	{
 		return VFS::Write(CurrentFiletable(), fd, buf, size);
 	}
 
-	off_t lseek(int fd, off_t offset, int whence)
+	off_t sys_lseek(int fd, off_t offset, int whence)
 	{
 		return VFS::Seek(CurrentFiletable(), fd, offset, whence);
 	}
 
-	int pipe(int pipefd[2])
+	int sys_pipe(int pipefd[2])
 	{
 		return VFS::CreatePipes(CurrentFiletable(), pipefd, 0);
 	}
 
-	int dup(int oldfd)
+	int sys_dup(int oldfd)
 	{
 		return VFS::DuplicateFile(CurrentFiletable(), oldfd);
 	}
 
-	int dup2(int oldfd, int newfd)
+	int sys_dup2(int oldfd, int newfd)
 	{
 		return VFS::DuplicateFile(CurrentFiletable(), oldfd, newfd);
 	}
 
-	int fcntl(int fd, int cmd, void *arg)
+	int sys_fcntl(int fd, int cmd, void *arg)
 	{
 		return VFS::Fcntl(CurrentFiletable(), fd, cmd, arg);
 	}
 
-	int getdents(int fd, dirent *dirp, unsigned int count)
+	int sys_getdents(int fd, dirent *dirp, unsigned int count)
 	{
 		return VFS::Getdents(CurrentFiletable(), fd, dirp, count);
 	}
 
-	pid_t fork()
+	pid_t sys_fork()
 	{
 		PROCESS *newproc = ProcessManager::Fork(Dispatcher::CurrentProcess());
 		return newproc ? newproc->id : -1;
 	}
 
-	pid_t getpid()
+	pid_t sys_getpid()
 	{
 		return Dispatcher::CurrentProcess()->id;
 	}
 
-	int execve(char const *path, char const *argv[], char const *envp[])
+	int sys_execve(char const *path, char const *argv[], char const *envp[])
 	{
 		PROCESS *proc = Dispatcher::CurrentProcess();
 		return ProcessManager::Exec(proc, path, argv, envp);
 	}
 
-	void *sbrk(intptr_t increment)
+	void *sys_sbrk(intptr_t increment)
 	{
 		PROCESS *proc = Dispatcher::CurrentProcess();
 		void *ret = ProcessManager::AdjustHeap(proc, increment);
@@ -131,7 +131,7 @@ namespace Syscalls
 		return (void *)-1;
 	}
 
-	int fd_stat(const char *file, stat *st)
+	int sys_stat(const char *file, stat *st)
 	{
 		if (file && !VFS::GetDentry(Path(file)))
 			return -ENOENT;
@@ -152,12 +152,12 @@ namespace Syscalls
 		return 0;
 	}
 
-	int fstat(int fd, stat *st)
+	int sys_fstat(int fd, stat *st)
 	{
-		return fd_stat(0, st);
+		return sys_stat(0, st);
 	}
 
-	int kill(pid_t pid, int signo)
+	int sys_kill(pid_t pid, int signo)
 	{
 		// TODO
 		if (pid < 0)
@@ -167,128 +167,128 @@ namespace Syscalls
 		return ProcessManager::HandleSignal(proc, signo);
 	}
 
-	sig_t signal(int signum, sig_t handler)
+	sig_t sys_signal(int signum, sig_t handler)
 	{
 		PROCESS *proc = Dispatcher::CurrentProcess();
 		return ProcessManager::SetSignalHandler(proc, signum, handler);
 	}
 
-	int waitpid(pid_t pid, int *status, int options)
+	int sys_waitpid(pid_t pid, int *status, int options)
 	{
 		return Dispatcher::Waitpid(pid, status, options);
 	}
 
-	int socket(int domain, int type, int protocol)
+	int sys_socket(int domain, int type, int protocol)
 	{
 		return VFS::CreateSocket(CurrentFiletable(), domain, type, protocol);
 	}
 
-	int accept(int fd, struct sockaddr *addr, socklen_t addrlen, int flags)
+	int sys_accept(int fd, struct sockaddr *addr, socklen_t addrlen, int flags)
 	{
 		return VFS::SocketAccept(CurrentFiletable(), fd, addr, addrlen, flags);
 	}
 
-	int bind(int fd, const sockaddr *addr, socklen_t addrlen)
+	int sys_bind(int fd, const sockaddr *addr, socklen_t addrlen)
 	{
 		return VFS::SocketBind(CurrentFiletable(), fd, addr, addrlen);
 	}
 
-	int connect(int fd, const struct sockaddr *addr, socklen_t addrlen)
+	int sys_connect(int fd, const struct sockaddr *addr, socklen_t addrlen)
 	{
 		return VFS::SocketConnect(CurrentFiletable(), fd, addr, addrlen);
 	}
 
-	int listen(int fd, int backlog)
+	int sys_listen(int fd, int backlog)
 	{
 		return VFS::SocketListen(CurrentFiletable(), fd, backlog);
 	}
 
-	int recv(int fd, void *buf, size_t len, int flags)
+	int sys_recv(int fd, void *buf, size_t len, int flags)
 	{
 		return VFS::SocketRecv(CurrentFiletable(), fd, buf, len, flags);
 	}
 
-	int recvfrom(int fd, void *buf, size_t len, int flags, sockaddr *src_addr, socklen_t addrlen)
+	int sys_recvfrom(int fd, void *buf, size_t len, int flags, sockaddr *src_addr, socklen_t addrlen)
 	{
 		return VFS::SocketRecvfrom(CurrentFiletable(), fd, buf, len, flags, src_addr, addrlen);
 	}
 
-	int send(int fd, const void *buf, size_t len, int flags)
+	int sys_send(int fd, const void *buf, size_t len, int flags)
 	{
 		return VFS::SocketSend(CurrentFiletable(), fd, buf, len, flags);
 	}
 
-	int sendto(int fd, const void *buf, size_t len, int flags, const sockaddr *dest_addr, socklen_t addrlen)
+	int sys_sendto(int fd, const void *buf, size_t len, int flags, const sockaddr *dest_addr, socklen_t addrlen)
 	{
 		return VFS::SocketSendto(CurrentFiletable(), fd, buf, len, flags, dest_addr, addrlen);
 	}
 
-	int shutdown(int fd, int how)
+	int sys_shutdown(int fd, int how)
 	{
 		return VFS::SocketShutdown(CurrentFiletable(), fd, how);
 	}
 
-	void halt()
+	void sys_halt()
 	{
 		sys_halt();
 	}
 
-	void print(char *text)
+	void sys_print(char *text)
 	{
 		while (*text)
 			debug_putc(*text++);
 	}
 
-	void color(uint32 color)
+	void sys_color(uint32 color)
 	{
 		debug_color(color);
 	}
 
-	void gettime(int &hour, int &minute, int &second)
+	void sys_gettime(int &hour, int &minute, int &second)
 	{
 		hour = RTC::Hour();
 		minute = RTC::Minute();
 		second = RTC::Second();
 	}
 
-	void draw(uint32 *framebuffer)
+	void sys_draw(uint32 *framebuffer)
 	{
 		Video::Draw(framebuffer);
 	}
 
-	void exit(int code)
+	void sys_exit(int code)
 	{
 		ProcessManager::Exit(Dispatcher::CurrentProcess(), code);
 	}
 
-	void exit_thread(int code)
+	void sys_exit_thread(int code)
 	{
 		Scheduler::ExitThread(code, Dispatcher::CurrentThread());
 	}
 
-	int sleep(useconds_t usec)
+	int sys_sleep(useconds_t usec)
 	{
 		Scheduler::SleepThread(Timer::Ticks() + usec, Dispatcher::CurrentThread());
 		return 0;
 	}
 
-	uint32 get_ticks()
+	uint32 sys_get_ticks()
 	{
 		return Timer::Ticks();
 	}
 
-	bool get_last_key(KEYCODE &code, bool &pressed)
+	bool sys_get_last_key(KEYCODE &code, bool &pressed)
 	{
 		return Keyboard::GetLastKey(code, pressed);
 	}
 
-	bool alloc_shared(int proc_id, void *&addr1, void *&addr2, uint32 blocks)
+	bool sys_alloc_shared(int proc_id, void *&addr1, void *&addr2, uint32 blocks)
 	{
 		ADDRESS_SPACE other_space = ProcessManager::GetProcess(proc_id)->addr_space;
 		return VMem::UserAllocShared(other_space, addr1, addr2, blocks);
 	}
 
-	uint32 read_file(char *&buffer, char *filename)
+	uint32 sys_read_file(char *&buffer, char *filename)
 	{
 		char *kernel_buf;
 
@@ -299,7 +299,7 @@ namespace Syscalls
 		return size;
 	}
 
-	pid_t create_process(char *filename)
+	pid_t sys_create_process(char *filename)
 	{
 		PROCESS *proc = ProcessManager::Exec(filename);
 
@@ -309,19 +309,19 @@ namespace Syscalls
 		return -1;
 	}
 
-	void debug_reset()
+	void sys_debug_reset()
 	{
 		debug_clear(0xFF000000);
 	}
 
 	//
-	int set_message(MESSAGE_HANDLER handler)
+	int sys_set_message(MESSAGE_HANDLER handler)
 	{
 		Dispatcher::CurrentProcess()->message_handler = handler;
 		return 1;
 	}
 
-	void send_message(int proc_id, int type, char *buf, int size, bool async, int &id)
+	void sys_send_message(int proc_id, int type, char *buf, int size, bool async, int &id)
 	{
 		PROCESS *src_proc = Dispatcher::CurrentProcess();
 		PROCESS *dst_proc = ProcessManager::GetProcess(proc_id);
@@ -349,7 +349,7 @@ namespace Syscalls
 		}
 	}
 
-	void send_message_reponse(int msg_id, int type, char *buf, int size)
+	void sys_send_message_reponse(int msg_id, int type, char *buf, int size)
 	{
 		PROCESS *src_proc = Dispatcher::CurrentProcess();
 		TMP_MSG *msg = first_msg;
@@ -370,7 +370,7 @@ namespace Syscalls
 		}
 	}
 
-	bool get_message_reponse(int msg_id, USER_MESSAGE &response)
+	bool sys_get_message_reponse(int msg_id, USER_MESSAGE &response)
 	{
 		TMP_MSG *msg = first_msg;
 		TMP_MSG *prev = 0;
@@ -437,56 +437,56 @@ namespace Syscalls
 
 	bool Init()
 	{
-		InstallSyscall(SYSCALL_EXIT, (SYSCALL_HANDLER)exit);
-		InstallSyscall(SYSCALL_OPEN, (SYSCALL_HANDLER)open);
-		InstallSyscall(SYSCALL_CLOSE, (SYSCALL_HANDLER)close);
-		InstallSyscall(SYSCALL_READ, (SYSCALL_HANDLER)read);
-		InstallSyscall(SYSCALL_WRITE, (SYSCALL_HANDLER)write);
-		InstallSyscall(SYSCALL_LSEEK, (SYSCALL_HANDLER)lseek);
-		InstallSyscall(SYSCALL_PIPE, (SYSCALL_HANDLER)pipe);
-		InstallSyscall(SYSCALL_DUP, (SYSCALL_HANDLER)dup);
-		InstallSyscall(SYSCALL_DUP2, (SYSCALL_HANDLER)dup2);
-		InstallSyscall(SYSCALL_FCNTL, (SYSCALL_HANDLER)fcntl);
-		InstallSyscall(SYSCALL_GETDENTS, (SYSCALL_HANDLER)getdents);
-		InstallSyscall(SYSCALL_FORK, (SYSCALL_HANDLER)fork);
-		InstallSyscall(SYSCALL_GETPID, (SYSCALL_HANDLER)getpid);
-		InstallSyscall(SYSCALL_EXECVE, (SYSCALL_HANDLER)execve);
-		InstallSyscall(SYSCALL_SBRK, (SYSCALL_HANDLER)sbrk);
-		InstallSyscall(SYSCALL_STAT, (SYSCALL_HANDLER)fd_stat);
-		InstallSyscall(SYSCALL_FSTAT, (SYSCALL_HANDLER)fstat);
-		InstallSyscall(SYSCALL_KILL, (SYSCALL_HANDLER)kill);
-		InstallSyscall(SYSCALL_SIGNAL, (SYSCALL_HANDLER)signal);
-		InstallSyscall(SYSCALL_WAITPID, (SYSCALL_HANDLER)waitpid);
+		InstallSyscall(SYSCALL_EXIT, (SYSCALL_HANDLER)sys_exit);
+		InstallSyscall(SYSCALL_OPEN, (SYSCALL_HANDLER)sys_open);
+		InstallSyscall(SYSCALL_CLOSE, (SYSCALL_HANDLER)sys_close);
+		InstallSyscall(SYSCALL_READ, (SYSCALL_HANDLER)sys_read);
+		InstallSyscall(SYSCALL_WRITE, (SYSCALL_HANDLER)sys_write);
+		InstallSyscall(SYSCALL_LSEEK, (SYSCALL_HANDLER)sys_lseek);
+		InstallSyscall(SYSCALL_PIPE, (SYSCALL_HANDLER)sys_pipe);
+		InstallSyscall(SYSCALL_DUP, (SYSCALL_HANDLER)sys_dup);
+		InstallSyscall(SYSCALL_DUP2, (SYSCALL_HANDLER)sys_dup2);
+		InstallSyscall(SYSCALL_FCNTL, (SYSCALL_HANDLER)sys_fcntl);
+		InstallSyscall(SYSCALL_GETDENTS, (SYSCALL_HANDLER)sys_getdents);
+		InstallSyscall(SYSCALL_FORK, (SYSCALL_HANDLER)sys_fork);
+		InstallSyscall(SYSCALL_GETPID, (SYSCALL_HANDLER)sys_getpid);
+		InstallSyscall(SYSCALL_EXECVE, (SYSCALL_HANDLER)sys_execve);
+		InstallSyscall(SYSCALL_SBRK, (SYSCALL_HANDLER)sys_sbrk);
+		InstallSyscall(SYSCALL_STAT, (SYSCALL_HANDLER)sys_stat);
+		InstallSyscall(SYSCALL_FSTAT, (SYSCALL_HANDLER)sys_fstat);
+		InstallSyscall(SYSCALL_KILL, (SYSCALL_HANDLER)sys_kill);
+		InstallSyscall(SYSCALL_SIGNAL, (SYSCALL_HANDLER)sys_signal);
+		InstallSyscall(SYSCALL_WAITPID, (SYSCALL_HANDLER)sys_waitpid);
 
-		InstallSyscall(SYSCALL_SOCKET, (SYSCALL_HANDLER)socket);
-		InstallSyscall(SYSCALL_ACCEPT, (SYSCALL_HANDLER)accept);
-		InstallSyscall(SYSCALL_BIND, (SYSCALL_HANDLER)bind);
-		InstallSyscall(SYSCALL_CONNECT, (SYSCALL_HANDLER)connect);
-		InstallSyscall(SYSCALL_LISTEN, (SYSCALL_HANDLER)listen);
-		InstallSyscall(SYSCALL_RECV, (SYSCALL_HANDLER)recv);
-		InstallSyscall(SYSCALL_RECVFROM, (SYSCALL_HANDLER)recvfrom);
-		InstallSyscall(SYSCALL_SEND, (SYSCALL_HANDLER)send);
-		InstallSyscall(SYSCALL_SENDTO, (SYSCALL_HANDLER)sendto);
-		InstallSyscall(SYSCALL_SHUTDOWN, (SYSCALL_HANDLER)shutdown);
+		InstallSyscall(SYSCALL_SOCKET, (SYSCALL_HANDLER)sys_socket);
+		InstallSyscall(SYSCALL_ACCEPT, (SYSCALL_HANDLER)sys_accept);
+		InstallSyscall(SYSCALL_BIND, (SYSCALL_HANDLER)sys_bind);
+		InstallSyscall(SYSCALL_CONNECT, (SYSCALL_HANDLER)sys_connect);
+		InstallSyscall(SYSCALL_LISTEN, (SYSCALL_HANDLER)sys_listen);
+		InstallSyscall(SYSCALL_RECV, (SYSCALL_HANDLER)sys_recv);
+		InstallSyscall(SYSCALL_RECVFROM, (SYSCALL_HANDLER)sys_recvfrom);
+		InstallSyscall(SYSCALL_SEND, (SYSCALL_HANDLER)sys_send);
+		InstallSyscall(SYSCALL_SENDTO, (SYSCALL_HANDLER)sys_sendto);
+		InstallSyscall(SYSCALL_SHUTDOWN, (SYSCALL_HANDLER)sys_shutdown);
 
-		InstallSyscall(SYSCALL_HALT, (SYSCALL_HANDLER)halt);
-		InstallSyscall(SYSCALL_PRINT, (SYSCALL_HANDLER)print);
-		InstallSyscall(SYSCALL_COLOR, (SYSCALL_HANDLER)color);
-		InstallSyscall(SYSCALL_GETTIME, (SYSCALL_HANDLER)gettime);
-		InstallSyscall(SYSCALL_DRAW, (SYSCALL_HANDLER)draw);
-		InstallSyscall(SYSCALL_EXIT_THREAD, (SYSCALL_HANDLER)exit_thread);
-		InstallSyscall(SYSCALL_SLEEP, (SYSCALL_HANDLER)sleep);
-		InstallSyscall(SYSCALL_GET_TICKS, (SYSCALL_HANDLER)get_ticks);
-		InstallSyscall(SYSCALL_GET_LAST_KEY, (SYSCALL_HANDLER)get_last_key);
-		InstallSyscall(SYSCALL_ALLOC_SHARED, (SYSCALL_HANDLER)alloc_shared);
-		InstallSyscall(SYSCALL_READ_FILE, (SYSCALL_HANDLER)read_file);
-		InstallSyscall(SYSCALL_CREATE_PROCESS, (SYSCALL_HANDLER)create_process);
-		InstallSyscall(SYSCALL_DEBUG_RESET, (SYSCALL_HANDLER)debug_reset);
+		InstallSyscall(SYSCALL_HALT, (SYSCALL_HANDLER)sys_halt);
+		InstallSyscall(SYSCALL_PRINT, (SYSCALL_HANDLER)sys_print);
+		InstallSyscall(SYSCALL_COLOR, (SYSCALL_HANDLER)sys_color);
+		InstallSyscall(SYSCALL_GETTIME, (SYSCALL_HANDLER)sys_gettime);
+		InstallSyscall(SYSCALL_DRAW, (SYSCALL_HANDLER)sys_draw);
+		InstallSyscall(SYSCALL_EXIT_THREAD, (SYSCALL_HANDLER)sys_exit_thread);
+		InstallSyscall(SYSCALL_SLEEP, (SYSCALL_HANDLER)sys_sleep);
+		InstallSyscall(SYSCALL_GET_TICKS, (SYSCALL_HANDLER)sys_get_ticks);
+		InstallSyscall(SYSCALL_GET_LAST_KEY, (SYSCALL_HANDLER)sys_get_last_key);
+		InstallSyscall(SYSCALL_ALLOC_SHARED, (SYSCALL_HANDLER)sys_alloc_shared);
+		InstallSyscall(SYSCALL_READ_FILE, (SYSCALL_HANDLER)sys_read_file);
+		InstallSyscall(SYSCALL_CREATE_PROCESS, (SYSCALL_HANDLER)sys_create_process);
+		InstallSyscall(SYSCALL_DEBUG_RESET, (SYSCALL_HANDLER)sys_debug_reset);
 
-		InstallSyscall(SYSCALL_SET_MESSAGE, (SYSCALL_HANDLER)set_message);
-		InstallSyscall(SYSCALL_SEND_MESSAGE, (SYSCALL_HANDLER)send_message);
-		InstallSyscall(SYSCALL_SEND_MESSAGE_RESPONSE, (SYSCALL_HANDLER)send_message_reponse);
-		InstallSyscall(SYSCALL_GET_MESSAGE_RESPONSE, (SYSCALL_HANDLER)get_message_reponse);
+		InstallSyscall(SYSCALL_SET_MESSAGE, (SYSCALL_HANDLER)sys_set_message);
+		InstallSyscall(SYSCALL_SEND_MESSAGE, (SYSCALL_HANDLER)sys_send_message);
+		InstallSyscall(SYSCALL_SEND_MESSAGE_RESPONSE, (SYSCALL_HANDLER)sys_send_message_reponse);
+		InstallSyscall(SYSCALL_GET_MESSAGE_RESPONSE, (SYSCALL_HANDLER)sys_get_message_reponse);
 
 		return true;
 	}
