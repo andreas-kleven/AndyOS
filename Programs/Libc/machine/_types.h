5abcf8cddb868608937872a2107e80ab379c252e
Andreas Kleven <andreas.kleven@live.no>
2020-07-28 12:23:39 +0200

    Stat types

---
+++ Programs/Libc/machine/_types.h
@@ -0,0 +1,26 @@
+#ifndef _MACHINE__TYPES_H
+#define _MACHINE__TYPES_H
+#include <machine/_default_types.h>
+
+typedef __int32_t __blkcnt_t;
+#define __machine_blkcnt_t_defined
+
+typedef __int32_t __blksize_t;
+#define __machine_blksize_t_defined
+
+typedef __int32_t __dev_t;
+#define __machine_dev_t_defined
+
+typedef __uint64_t __ino_t;
+#define __machine_ino_t_defined
+
+typedef __uint32_t __mode_t;
+#define __machine_mode_t_defined
+
+typedef __uint32_t __uid_t;
+#define __machine_uid_t_defined
+
+typedef __uint32_t __gid_t;
+#define __machine_gid_t_defined
+
+#endif
