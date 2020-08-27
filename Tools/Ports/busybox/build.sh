a99c40ad1d66760eb58c2aed6efc8cff8910fe1c
Andreas Kleven <andreas.kleven@live.no>
2020-08-27 18:14:16 +0200

    Busybox port

---
+++ Tools/Ports/busybox/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+builddir=$PWD/build
+destdir=$PWD/../../../Root
+mkdir $builddir
+
+git clone -b 1_32_stable --single-branch git://busybox.net/busybox.git src
+cd src
+git apply --ignore-space-change --ignore-whitespace ../patches.patch
+
+cp ../.config ./
+make busybox install
+cp _install/bin/* $destdir/bin
+cp _install/usr/bin/* $destdir/bin
