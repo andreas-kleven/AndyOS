1331858c830c7840431d3439a632e4b83375286b
Andreas Kleven <andreas.kleven@live.no>
2020-11-08 15:40:34 +0100

    sse4

--- Tools/gdb-x86.sh
+++ Tools/gdb-x86.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 GNOME_TERMINAL_SCREEN=""
 mkfifo /tmp/guest
-gnome-terminal -- qemu-system-i386 -m 1G -cdrom ../AndyOS.iso -drive file="$HOME/VirtualBox VMs/AndyOS/disk.img" -s -S -serial pipe:/tmp/guest -device AC97
+gnome-terminal -- qemu-system-i386 -m 1G -cpu qemu32,+ssse3,+sse4.1,+sse4.2, -cdrom ../AndyOS.iso -drive file="$HOME/VirtualBox VMs/AndyOS/disk.img" -s -S -serial pipe:/tmp/guest -device AC97
 
 sleep 0.1
 
